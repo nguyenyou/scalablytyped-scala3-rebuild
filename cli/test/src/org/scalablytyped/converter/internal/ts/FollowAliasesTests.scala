b762b35950ae54d8d1f6c2a9d4b76883b44ed0a2
nguyenyou <tunt081295@gmail.com>
2025-09-05 10:48:41 +0700

    🚢 ship it

--- cli/test/src/org/scalablytyped/converter/internal/ts/FollowAliasesTests.scala
+++ cli/test/src/org/scalablytyped/converter/internal/ts/FollowAliasesTests.scala
@@ -69,9 +69,121 @@ object FollowAliasesTests extends TestSuite {
       test("returns original type when no alias found") {
         val scope = createMockScope()
         val typeRef = TsTypeRef(createQIdent("UnknownType"))
-        
+
         val result = FollowAliases(scope)(typeRef)
-        
+
+        assert(result == typeRef)
+      }
+
+      test("follows simple type alias") {
+        val alias = createMockTypeAlias("StringAlias", TsTypeRef.string)
+        val scope = createMockScope(alias)
+        val typeRef = TsTypeRef(createQIdent("StringAlias"))
+
+        val result = FollowAliases(scope)(typeRef)
+
+        assert(result == TsTypeRef.string)
+      }
+
+      test("follows nested type alias") {
+        val innerAlias = createMockTypeAlias("InnerAlias", TsTypeRef.number)
+        val outerAlias = createMockTypeAlias("OuterAlias", TsTypeRef(createQIdent("InnerAlias")))
+        val scope = createMockScope(innerAlias, outerAlias)
+        val typeRef = TsTypeRef(createQIdent("OuterAlias"))
+
+        val result = FollowAliases(scope)(typeRef)
+
+        assert(result == TsTypeRef.number)
+      }
+
+      test("follows thin interface") {
+        val thinInterface = createMockInterface("ThinInterface", members = Empty)
+        val scope = createMockScope(thinInterface)
+        val typeRef = TsTypeRef(createQIdent("ThinInterface"))
+
+        val result = FollowAliases(scope)(typeRef)
+
+        // Should return the interface as a type reference since it's thin
+        assert(result.isInstanceOf[TsTypeRef])
+      }
+
+      test("does not follow thick interface") {
+        val property = TsMemberProperty(
+          comments = NoComments,
+          level = TsProtectionLevel.Default,
+          name = createSimpleIdent("prop"),
+          tpe = Some(TsTypeRef.string),
+          expr = None,
+          isStatic = false,
+          isReadOnly = false
+        )
+        val thickInterface = createMockInterface("ThickInterface", members = IArray(property))
+        val scope = createMockScope(thickInterface)
+        val typeRef = TsTypeRef(createQIdent("ThickInterface"))
+
+        val result = FollowAliases(scope)(typeRef)
+
+        // Should return original type since interface is not thin
+        assert(result == typeRef)
+      }
+    }
+
+    test("FollowAliases - union and intersection types") {
+      test("follows aliases in union types") {
+        val alias1 = createMockTypeAlias("Alias1", TsTypeRef.string)
+        val alias2 = createMockTypeAlias("Alias2", TsTypeRef.number)
+        val scope = createMockScope(alias1, alias2)
+
+        val unionType = TsTypeUnion(IArray(
+          TsTypeRef(createQIdent("Alias1")),
+          TsTypeRef(createQIdent("Alias2"))
+        ))
+
+        val result = FollowAliases(scope)(unionType)
+
+        assert(result.isInstanceOf[TsTypeUnion])
+        val resultUnion = result.asInstanceOf[TsTypeUnion]
+        assert(resultUnion.types.contains(TsTypeRef.string))
+        assert(resultUnion.types.contains(TsTypeRef.number))
+      }
+
+      test("follows aliases in intersection types") {
+        val alias1 = createMockTypeAlias("Alias1", TsTypeRef.string)
+        val alias2 = createMockTypeAlias("Alias2", TsTypeRef.number)
+        val scope = createMockScope(alias1, alias2)
+
+        val intersectionType = TsTypeIntersect(IArray(
+          TsTypeRef(createQIdent("Alias1")),
+          TsTypeRef(createQIdent("Alias2"))
+        ))
+
+        val result = FollowAliases(scope)(intersectionType)
+
+        assert(result.isInstanceOf[TsTypeIntersect])
+        val resultIntersection = result.asInstanceOf[TsTypeIntersect]
+        assert(resultIntersection.types.contains(TsTypeRef.string))
+        assert(resultIntersection.types.contains(TsTypeRef.number))
+      }
+    }
+
+    test("FollowAliases - typeRef method") {
+      test("typeRef returns TsTypeRef for simple alias") {
+        val alias = createMockTypeAlias("StringAlias", TsTypeRef.string)
+        val scope = createMockScope(alias)
+        val typeRef = TsTypeRef(createQIdent("StringAlias"))
+
+        val result = FollowAliases.typeRef(scope)(typeRef)
+
+        assert(result.isInstanceOf[TsTypeRef])
+        assert(result == TsTypeRef.string)
+      }
+
+      test("typeRef returns original for unknown type") {
+        val scope = createMockScope()
+        val typeRef = TsTypeRef(createQIdent("UnknownType"))
+
+        val result = FollowAliases.typeRef(scope)(typeRef)
+
         assert(result == typeRef)
       }
     }
